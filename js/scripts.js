c39928900e4453c3dd12ac27cb147c5de124d740
alissa paluska and polina nenchev <alissa.paluska@gmail.com>
2016-01-21 09:12:30 -0800

    Loop to a given number

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var count = function(number) {
+
+  for(var i=0; i <= number; i++) {
+    console.log(i);
+  }
+  return true;
+};
---
+++ spec/specs.js
@@ -0,0 +1,20 @@
+describe('count',function(){
+   it("will return true, if output of number reaches 30", function() {
+     expect(count(30)).to.equal(true);
+   });
+
+
+
+
+
+
+
+
+
+
+  // it("will return false for a number greater than 30",function(){
+  //     expect(count(30)).to.equal(false);
+  // });
+
+
+});
