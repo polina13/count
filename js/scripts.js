e70fbfcf1cda5426fc854aefc8d9a686843ac815
alissa paluska and polina nenchev <polina.mihova@yahoo.com>
2016-01-21 10:50:33 -0800

    add functionality, need to alter spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
-var count = function(number) {
 
-  for(var i=0; i <= number; i++) {
-    console.log(i);
+var count = function(num1,num2) {
+  var array = [];
+  for (var i = num2; i <= num1; i = i+num2) {
+    array.push(i + "");
   }
-  return true;
+  if (num2 > num1 ){
+    return "error";
+  }else{
+    return array;
+}
 };
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+  var num1 = parseInt($("input#num1").val());
+  var num2 = parseInt($("input#num2").val());
+  var result = count(num1,num2);
+  console.log(result);
+  $("#output").text(result);
+
+  event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,20 +1,12 @@
 describe('count',function(){
-   it("will return true, if output of number reaches 30", function() {
-     expect(count(30)).to.equal(true);
+   it("will return true, if output of number reaches 10", function() {
+     expect(count(10)).to.eql([1,2,3,4,5,6,7,8,9,10]);
    });
 
-
-
-
-
-
-
-
-
-
-  // it("will return false for a number greater than 30",function(){
-  //     expect(count(30)).to.equal(false);
-  // });
-
-
+   it("will return true, if output of number reaches 30 and it increments by 5", function() {
+     expect(count(30,5)).to.eql([5,10,15,20,25,30]);
+   });
+  it("will return false for a number greater than original given number",function(){
+      expect(count(30,60)).to.equal("error");
+  });
 });
